b0122a5a7b5b8419ad23a8cf7e09903edf24f909
stefantasy <stefan1992@qq.com>
2018-08-27 15:40:57 +0800

    fix showshart shell

--- sfbot_bringup/show_start/启动颜色追踪color_track.sh
+++ sfbot_bringup/show_start/启动颜色追踪color_track.sh
@@ -6,7 +6,7 @@ gnome-terminal -t "播报服务" -x bash -c "roslaunch sfbot_bringup soundplay.l
 
 echo Open C V Camshift Tracking Mode|rosrun sound_play say.py
 
-gnome-terminal -t "启动rbcBot机器人" -x bash -c "roslaunch mx_bringup camera_start.launch ;exec bash;"
+gnome-terminal -t "启动rbcBot机器人" -x bash -c "roslaunch sfbot_bringup camera_start.launch ;exec bash;"
 
 sleep 2
 gnome-terminal -t "启动颜色追踪tracker" -x bash -c "roslaunch sfbot_apps opencv3_tracker.launch color:=true ;exec bash;"
